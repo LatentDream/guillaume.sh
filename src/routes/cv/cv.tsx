f915e7d31dcc4ed7d0d3e4da1157a4b80c5018bd
Guillaume Thibault <guillaume.thibault.98@gmail.com>
2024-04-19 15:22:40 -0400

    chore: job card & Eduction

--- src/routes/cv/cv.tsx
+++ src/routes/cv/cv.tsx
@@ -1,19 +1,63 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/job-card";
 import { Section } from "@/components/ui/section";
 import { EnvelopeClosedIcon, GitHubLogoIcon, GlobeIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
 
-function Cv() {
+const work = [
+  {
+    company: "Flojoy",
+    start: 2024,
+    end: "Now",
+    link: "https://flojoy.ai",
+    title: "Software Engineer / Product Designer",
+    description: "Originally hired as a Hardware and ML Engineer to extend the capabilities of Flojoy Studio (an open-source alternative to LabVIEW), I quickly transitioned into a role of Product Designer when the company pivoted to an adjacent market, tackling Test and Measurement automation for PCB board testing. Within 3 months, we developed a test sequencer for manufacturing and engineering. We provided a full-stack solution, comprising a desktop application for developing and running tests, along with a cloud solution for managing and visualizing the tests and data.",
+    badges: ["Product Design", "Typescript", "Python", "Hardware Design", "Project Management"],
+    notes: ["Reported directly to Jack Parmer, CEO of Flojoy and previously founder of Plotly", "Worked with two truly talented engineers: Jeff Zhang & Xiaowei (Joey) Yu"]
+  },
+  {
+    company: "Boreal Ventures",
+    start: 2024,
+    end: 2025,
+    link: "https://flojoy.ai",
+    title: "Hardware / ML engineer",
+    description: "Test",
+    badges: [""],
+    notes: []
+  },
+  {
+    company: "Boreal Ventures",
+    start: 2024,
+    end: 2025,
+    link: "https://flojoy.ai",
+    title: "Hardware / ML engineer",
+    description: "Test",
+    badges: [""],
+    notes: []
+  },
+  {
+    company: "Boreal Ventures",
+    start: 2024,
+    end: 2025,
+    link: "https://flojoy.ai",
+    title: "Hardware / ML engineer",
+    description: "Test",
+    badges: [""],
+    notes: []
+  },
+]
 
+
+function Cv() {
   return (
     <main className="container relative mx-auto scroll-my-12 overflow-auto p-4 print:p-12 md:p-16">
       <section className="mx-auto w-full max-w-2xl space-y-8 bg-white print:space-y-6">
         <div className="flex items-center justify-between">
-          <div className="flex-1 space-y-1.5">
-            <h1 className="text-2xl font-bold">Guillaume Thibault</h1>
+          <div className="flex-none space-y-1.5">
+            <h1 className="text-2xl font-bold items-start">Guillaume Thibault</h1>
             <p className="max-w-md text-pretty font-mono text-sm text-muted-foreground">
-              ...
+              Software Engineer
             </p>
             <p className="max-w-md items-center text-pretty font-mono text-xs text-muted-foreground">
               <a
@@ -26,38 +70,38 @@ function Cv() {
               </a>
             </p>
             <div className="flex gap-x-1 pt-1 font-mono text-sm text-muted-foreground print:hidden">
-                <Button
-                  className="size-8"
-                  variant="outline"
-                  size="icon"
-                  asChild
-                >
-                  <a href={`mailto:guillaume.thibault.98@proton.me`}>
-                    <EnvelopeClosedIcon className="size-4" />
-                  </a>
-                </Button>
-                <Button
-                  key="Linkedin"
-                  className="size-8"
-                  variant="outline"
-                  size="icon"
-                  asChild
-                >
-                  <a href="https://www.linkedin.com/in/guillaume-thibault/">
-                    <LinkedInLogoIcon className="size-4" />
-                  </a>
-                </Button>
-                <Button
-                  key="Github"
-                  className="size-8"
-                  variant="outline"
-                  size="icon"
-                  asChild
-                >
-                  <a href="https://github.com/latentDream/">
-                    <GitHubLogoIcon className="size-4" />
-                  </a>
-                </Button>
+            <Button
+              className="size-8"
+              variant="outline"
+              size="icon"
+              asChild
+            >
+              <a href={`mailto:guillaume.thibault.98@proton.me`}>
+                <EnvelopeClosedIcon className="size-4" />
+              </a>
+            </Button>
+            <Button
+              key="Linkedin"
+              className="size-8"
+              variant="outline"
+              size="icon"
+              asChild
+            >
+              <a href="https://www.linkedin.com/in/guillaume-thibault/">
+                <LinkedInLogoIcon className="size-4" />
+              </a>
+            </Button>
+            <Button
+              key="Github"
+              className="size-8"
+              variant="outline"
+              size="icon"
+              asChild
+            >
+              <a href="https://github.com/latentDream/">
+                <GitHubLogoIcon className="size-4" />
+              </a>
+            </Button>
             </div>
             <div className="hidden flex-col gap-x-1 font-mono text-sm text-muted-foreground print:flex">
                 <a href={`mailto:guillaume.thibault.98@proton.me`}>
@@ -74,34 +118,116 @@ function Cv() {
         <Section>
           <h2 className="text-xl font-bold">About</h2>
           <p className="text-pretty font-mono text-sm text-muted-foreground">
-            ...
+            Hi! I'm Guillaume :) Passionate about problem-solving, system building, and art. My journey began with a bachelor's degree in software engineering, where I explored the world of applied mathematics and learned the intricacies of computer systems. Fascinated by their complexity, I recently completed a professional master's degree in artificial intelligence and data science.
           </p>
         </Section>
         <Section>
           <h2 className="text-xl font-bold">Work Experience</h2>
-            <p> ... </p>
+          {work.map((work) => {
+            return (
+              <Card key={work.company}>
+                <CardHeader>
+                  <div className="flex items-center justify-between gap-x-2 text-base">
+                    <h3 className="inline-flex items-center justify-center gap-x-1 font-semibold leading-none">
+                      <a className="hover:underline" href={work.link}>
+                        {work.company}
+                      </a>
+                   </h3>
+                    <div className="text-sm tabular-nums text-gray-500">
+                      {work.start} - {work.end}
+                    </div>
+                  </div>
+
+                  <h4 className="font-mono text-sm leading-none">
+                    {work.title}
+                  </h4>
+                </CardHeader>
+                <CardContent className="mt-2 text-xs">
+                  {work.description}
+                </CardContent>
+                {work.notes.map((note) => (
+                  <CardContent variant="note">
+                    -{" "}{ note }
+                  </CardContent>
+                ))}
+                <div className="flex flex-wrap gap-2 mt-1">
+                  {work.badges.map((badge) => (
+                    <Badge
+                      variant="secondary"
+                      className="align-middle text-xs"
+                      key={badge}
+                    >
+                      {badge}
+                    </Badge>
+                  ))}
+                </div>
+              </Card>
+            );
+          })}
         </Section>
         <Section>
           <h2 className="text-xl font-bold">Education</h2>
-            <p> ... </p>
+            <div className="grid grid-cols-2 gap-4">
+              <Card>
+                <CardHeader>
+                  <h3 className="gap-x-1 font-semibold">
+                    Bachelor's degree Software Engineering
+                  </h3>
+                  <h4 className="font-mono text-sm leading-none">
+                    Polytechnique Montreal
+                  </h4>
+                </CardHeader>
+                <CardContent className="mt-2 text-xs">
+                  With a minor in finance with a focus on sustainable investment & innovation; Enriched by 3 master courses and two international seminars in Europe and Japan. Promoted data science through the organization of the first two editions of the Polytechnique data science hackathon. More than 50 participants in graduate study per edition with more than 8 corporate sponsors.
+                </CardContent>
+              </Card>
+              <Card>
+                <CardHeader>
+                  <h3 className="gap-x-1 font-semibold">
+                    Master's Degree in Computer Engineering.
+                  </h3>
+                  <h4 className="font-mono text-sm leading-none">
+                    Polytechnique Montreal
+                  </h4>
+                </CardHeader>
+                <CardContent className="mt-2 text-xs">
+                  Professional Master to get up to speed with artificial intelligence research as well as to discover interesting fields of application. Project done with a pension fund to introduce methods for analyzing the sentiment of central bank texts in a currency hedging model while being able to have a form of explicability of results.
+                </CardContent>
+              </Card>
+            </div>
         </Section>
         <Section>
           <h2 className="text-xl font-bold">Skills</h2>
+          <h3 className="text-md font-bold">Programming Languages</h3>
           <div className="flex flex-wrap gap-1">
-              <Badge>Python</Badge>;
-              <Badge>TypeScript</Badge>;
-              <Badge>Rust</Badge>;
+            <Badge>Python</Badge>
+            <Badge>TypeScript</Badge>
+            <Badge>Rust</Badge>
+            <Badge>Java</Badge>
+            <Badge>Machine Learning</Badge>
+            <Badge>Deep Learning</Badge>
+            <Badge>Data Science</Badge>
+          </div>
+          <h3 className="text-md font-bold">Other</h3>
+          <div className="flex flex-wrap gap-1"> 
+            <Badge>Product Design</Badge>
+            <Badge>UI/UX</Badge>
+            <Badge>Project Management</Badge>
+            <Badge>"Public" Speaking</Badge>
+            <Badge>French</Badge>
+            <Badge>English</Badge>
           </div>
         </Section>
 
         <Section className="print-force-new-page scroll-mb-16">
           <h2 className="text-xl font-bold">Projects</h2>
           <div className="-mx-3 grid grid-cols-1 gap-3 print:grid-cols-3 print:gap-2 md:grid-cols-2 lg:grid-cols-3">
-            "Project"
+            <p> ... </p>
+            <p> ... </p>
+            <p> ... </p>
           </div>
         </Section>
       </section>
-
     </main>
   )
 }
