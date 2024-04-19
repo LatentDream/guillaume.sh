e7bda5bb26b8627358f0a63a5df2e6fdecc2cf5c
Guillaume Thibault <guillaume.thibault.98@gmail.com>
2024-04-19 11:39:59 -0400

    install shadcui

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from '@/components/ui/button'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -31,6 +33,7 @@ function App() {
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
+      <Button> Click me </Button>
     </>
   )
 }
