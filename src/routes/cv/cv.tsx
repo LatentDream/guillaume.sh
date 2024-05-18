68fe7509a41a8d63a3a270208989db8428fa51c5
Gui <guillaume.thibault.98@proton.me>
2024-05-17 22:12:16 -0400

    chore: better explanation

--- src/routes/cv/cv.tsx
+++ src/routes/cv/cv.tsx
@@ -78,7 +78,7 @@ const internships = [
 const projects = [
   {
     title: "Art Commun",
-    description: "[WORK IN PROGRESS] Visit ArtCommun.com for the full dev log, but the end goal here is to learn to build web stuff, without the normal web tech. And maybe having a simple game maybe in the browser in the end. Currently the backend is Rust, which will serve the account state, and the frontend might be in C ? Not sure, but I'm currently learning 3D rendering from scratch with C, and WebAssembly is next.",
+    description: "[WORK IN PROGRESS] The primary objective of this project is to explore web development using "unconventional" technologies, with the aim of creating a simple game (still not sure of the final desired output) that can run in a browser. Currently, the backend is done using Rust to handle account states and persistence, and the frontend might be implemented in C, though this is still under consideration. The current focus is to do 3D rendering from scratch with C, with plans to integrate WebAssembly next. Stay tuned for updates as the project evolves! For a full dev log, visit ArtCommun.com.",
     link: "https://artcommun.com",
     imgLink: [],
     techs: ["Rust", "Postgres", "C", "Railway"],
